294defe966946c0d0f931c1d44813f984e362784
Haruto Mori <baystarsman@outlook.jp>
2023-10-14 12:02:24 +0900

    type conversion

---
+++ type-conversion/convert-types.js
@@ -0,0 +1,10 @@
+let age = window.prompt("How old are you?");
+console.log(typeof age);
+
+// convert to number
+age = Number(age);
+console.log(typeof age);
+
+// convert to string
+age = String(age)
+console.log(typeof age)
