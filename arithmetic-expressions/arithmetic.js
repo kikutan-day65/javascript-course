905f2a7a2edd210d38d05a789c30d9c4db7f2b3e
Haruto Mori <baystarsman@outlook.jp>
2023-10-14 11:40:10 +0900

    arithemetic expressions

---
+++ arithmetic-expressions/arithmetic.js
@@ -0,0 +1,20 @@
+let students = 20;
+let teachers = 10;
+let total_people;
+
+students += 1
+teachers -= 5
+total_people = students + teachers
+
+console.log("num of students:", students)
+console.log("num of teachers:", teachers)
+console.log("total:", total_people)
+
+
+let apple = 5;
+let price = 100;
+let discount = 10;
+
+let total_price = (apple * price) * ((100 - discount) / 100)
+
+console.log("total price:", total_price)
